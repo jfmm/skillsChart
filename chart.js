4d15e494e81e423d058dcc92372342a98f2b6b44
jfmm <moralejf@gmail.com>
2016-10-05 22:55:49 -0400

    g elements are being added now at least

--- chart.js
+++ chart.js
@@ -1,14 +1,61 @@
 var skillPointData = "https://teamtreehouse.com/josemorales.json";
 
-var chart = d3.select("#chart-wrapper")
-                    .append('svg')
-                    .attr('id', 'chart');
+// set dimensions
+var width = 500,
+    barHeight = 20;
 
+// set x scale
+var xScale = d3.scaleLinear()
+            .range([0, width]);
 
+var chart = d3.select("#chart")
+              .attr("width", width);
 
 // get data
-d3.json(skillPointData, function(data) {
+d3.json(skillPointData, function(error, data) {
        
-       console.log(data.points);
+  if(error) throw new Error("data not found");
+  
+  
+  var dataArray = [];
 
-     });
+  var dataLength = Object.keys(data.points).length - 1; // -1 because we don't want to graph the "total" property
+
+
+  // iterate over API object and create an array for easier mapping later on.
+  for ( var property in data.points) {
+
+    var item = {
+        skillName : property,
+        points : data.points[property]
+    };
+    
+    dataArray.push(item);
+  }
+  
+  console.log(dataArray);
+  
+  
+  
+  // set domain  
+  xScale.domain([0, d3.max(data, function(d) { return d.value; })]);
+ 
+  // Set chart height dynamically
+  chart.attr("height", barHeight * dataLength);
+
+  
+  //var i = 0;
+  var bar = chart.selectAll("g")
+    .data(dataArray)
+    .enter().append("g")
+    // whats this
+    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+  
+  //TODO: ERROR HERE.
+  bar.append("rect")
+      .attr("width", function(d) { 
+    return xScale(d); 
+  })
+    .attr("height", barHeight - 1);
+  
+});
